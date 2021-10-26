f5f07acf88e7ede3f50b4711e35130ef74976d6d
aoyama_saito <saitomarimo1229@gmail.com>
2021-10-26 17:15:53 +0900

    NavMesh導入 上手くいかんぞ

--- sinsakai3 FPS/Assets/Script/Enemy/EnemyBase.cs
+++ sinsakai3 FPS/Assets/Script/Enemy/EnemyBase.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyBase : MonoBehaviour
 {
+    //Chaseの変数
     [Header("ステータス")]
     [SerializeField] int hp = 10;
     [SerializeField] float chaseSpeed = 4;
@@ -18,35 +21,44 @@ public class EnemyBase : MonoBehaviour
     [Header("プレイヤー追跡パターン")]
     [SerializeField]EnemyPatern state = default;
 
+    //Prowlの変数
+    GameObject[] targets;
+    int targetIndex = 0;
+    Collider collision;
+
     RaycastHit hit;
     Rigidbody rb;
+    NavMeshAgent navAgent;
     GameObject player;
     float kyori = 0;
     float timer = 0;
     float chaseHeight = 0;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-
-        player = GameObject.FindGameObjectWithTag("Player");
-
-        chaseHeight = Random.Range(minHeight, maxHeight);
-        Debug.Log(chaseHeight);
-
-        transform.position = new Vector3(gameObject.transform.position.x, chaseHeight, gameObject.transform.position.z);       
+        switch (state)
+        {
+            case EnemyPatern.chase:
+                rb = GetComponent<Rigidbody>();
+                chaseHeight = Random.Range(minHeight, maxHeight);
+                transform.position = new Vector3(gameObject.transform.position.x, chaseHeight, gameObject.transform.position.z);
+                break;
+            case EnemyPatern.prowl:
+                navAgent = GetComponent<NavMeshAgent>();
+                targets = GameObject.FindGameObjectsWithTag("NavTarget");
+                targetIndex = Random.Range(0, targets.Length - 1);
+                navAgent.destination = targets[targetIndex].transform.position;
+                break;
+        }            
     }
 
     void FixedUpdate()
     {
-        switch(state)
+        switch (state)
         {
             case EnemyPatern.chase:
                 Chase();
                 break;
-            case EnemyPatern.prowl:
-                Prowl();
-                break;               
-        }       
+        }
     }
     /// <summary>
     /// 引数にダメージを設定する
@@ -96,9 +108,18 @@ public class EnemyBase : MonoBehaviour
         }
     }
 
-    void Prowl()
+    void OnTriggerEnter(Collider other)
     {
+        Prowl(other);
+    }
+    void Prowl(Collider collision)
+    {
+        navAgent.destination = targets[targetIndex].transform.position;
 
+        if (collision.gameObject.tag == "NavTarget")
+        {
+            targetIndex = Random.Range(0, targets.Length - 1);
+        }
     }
     enum EnemyPatern
     {
