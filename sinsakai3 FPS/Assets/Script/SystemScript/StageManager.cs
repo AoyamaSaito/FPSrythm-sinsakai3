f4c8a18366e4f997835bfbd6632d11b5f0c2dff0
aoyama_saito <saitomarimo1229@gmail.com>
2021-11-18 10:08:23 +0900

    ステージマネージャー製作中

---
+++ sinsakai3 FPS/Assets/Script/SystemScript/StageManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField] GameObject firstStage;
+    [SerializeField] GameObject[] stages;
+
+    List<GameObject> inGameStages = new List<GameObject>();
+    GameObject[] shuffleStage;
+
+    void Start()
+    {
+        shuffleStage = stages.OrderBy(i => Guid.NewGuid()).ToArray();
+        inGameStages.Add(Instantiate(firstStage, Vector3.zero, Quaternion.identity));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
