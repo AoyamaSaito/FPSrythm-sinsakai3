29e79aaf994bd17e3e1a2ea8754487b553f9dae1
aoyama_saito <saitomarimo1229@gmail.com>
2021-11-20 19:29:17 +0900

    ステージマネージャー製作中

--- sinsakai3 FPS/Assets/Script/SystemScript/StageManager.cs
+++ sinsakai3 FPS/Assets/Script/SystemScript/StageManager.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    [SerializeField] GameObject firstStage;
-    [SerializeField] GameObject[] stages;
+    [SerializeField, Tooltip("最初に呼び出されるステージ")] GameObject firstStage;
+    [SerializeField, Tooltip("SetActiveをfalseにしてください")] GameObject[] stages;
 
     List<GameObject> inGameStages = new List<GameObject>();
     GameObject[] shuffleStage;
@@ -16,11 +16,14 @@ public class StageManager : MonoBehaviour
     {
         shuffleStage = stages.OrderBy(i => Guid.NewGuid()).ToArray();
         inGameStages.Add(Instantiate(firstStage, Vector3.zero, Quaternion.identity));
+        for(int i = 0; i < shuffleStage.Length; i++)
+        {
+            inGameStages.Add(Instantiate(shuffleStage[i], new Vector3(0, 0, 50 + 50 * i), Quaternion.identity));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void NextStage()
     {
-        
+
     }
 }
