9d4a83cdb489b5d6b8bdb71c517285a8397732e1
aoyama_saito <saitomarimo1229@gmail.com>
2021-12-20 16:52:19 +0900

    リスタート機能を追加

--- sinsakai3 FPS/Assets/Script/SystemScript/PauseManager.cs
+++ sinsakai3 FPS/Assets/Script/SystemScript/PauseManager.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CursorManager : MonoBehaviour
+public class PauseManager : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera cm;
+    [SerializeField] GameObject PausePanel;
 
+    bool isPause = false;
     void Start()
     {
         Cursor.visible = false;
@@ -18,7 +21,11 @@ public class CursorManager : MonoBehaviour
     {
         if (Input.GetButtonDown("Cancel"))
         {
+            PausePanel.SetActive(!isPause);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
             cm.enabled = !cm.enabled;
+            isPause = !isPause;
         }
     }
 }
