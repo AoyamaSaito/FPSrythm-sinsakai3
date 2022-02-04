5dcd54668e3e6ce574f44eecb67ea75679c1a644
aoyama_saito <saitomarimo1229@gmail.com>
2022-02-04 20:29:55 +0900

    色々追加

---
+++ sinsakai3 FPS/Assets/Script/Item/Ramdom/RamdomInstantiate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RamdomInstantiate : MonoBehaviour
+{
+    [SerializeField, Tooltip("生成するアイテム")] GameObject[] items;
+
+    [Tooltip("生成するアイテムのラベル")]　int number = 0;
+
+    void Start()
+    {
+        number = Random.Range(0, items.Length - 1);
+
+    }
+
+    public void Pop()
+    {
+        Instantiate(items[number], transform.position, Quaternion.identity);
+    }
+}
